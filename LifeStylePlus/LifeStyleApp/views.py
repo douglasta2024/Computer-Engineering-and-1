3dd7fedcbad4d09ee7b63f727573d46a49e14740
douglasta2024 <80347061+douglasta2024@users.noreply.github.com>
2022-11-07 14:35:08 -0500

    changed homepage

--- LifeStylePlus/LifeStyleApp/views.py
+++ LifeStylePlus/LifeStyleApp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def hi(request):
-    return render(request, 'LifeStyleApp/hi.html')
+    return render(request, 'LifeStyleApp/PhysicalDataInput.html')
