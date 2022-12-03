fceb7916fbadb9b1aed58804afa42694238b9a20
James Fesik <james.fesik@ufl.edu>
2022-12-03 17:30:31 -0500

    local branch changes

--- LifeStylePlus/LifeStyleApp/forms.py
+++ LifeStylePlus/LifeStyleApp/forms.py
@@ -1,9 +1,17 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Post
 from django import forms
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UploadForm(ModelForm):
+    miles = forms.IntegerField()
+    minutes = forms.IntegerField()
+    class Meta:
+        model = Post
+        fields = ['miles', 'minutes']
